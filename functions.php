6cb91424640fe7fbed12d33019219e8127a7e95a
Robin Sue <robin@decnalab.com>
2015-09-04 23:36:31 +0800

    Add Foundation scripts into wp template
    - Enqueue foundation css & js
    - Remove meta generator tag
    - Hide version numbers on scripts

--- functions.php
+++ functions.php
@@ -114,17 +114,73 @@ add_action( 'widgets_init', '_s_widgets_init' );
  * Enqueue scripts and styles.
  */
 function _s_scripts() {
-	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
+	// wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	// Register consolidated style sheet
+    wp_enqueue_style( '_s-css', get_template_directory_uri() . '/assets/css/style.css', array(), '', 'all' );
+
+	// wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+
+    // Loads Modernizr from bower_components
+    wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/bower_components/foundation/js/vendor/modernizr.js', array(), '2.8.3', false );
+
+	// Removes WP version of jQuery
+	wp_deregister_script('jquery');
+
+	// Loads jQuery from bower_components
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/bower_components/foundation/js/vendor/jquery.js', array(), '2.1.3', true );
+
+    // Adding Foundation scripts file in the footer
+   wp_enqueue_script( 'foundation-js', get_template_directory_uri() . '/assets/js/min/foundation.min.js', array( 'jquery' ), '', true );
+    
+    // Adding custom scripts file in the footer
+    wp_enqueue_script( '_s-js', get_template_directory_uri() . '/assets/js/app.js', array( 'jquery' ), '', true );
 
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
+
+
+
+/**
+ * Remove Meta Generator
+ */
+function disable_version() {
+return ”;
+}
+add_filter('the_generator','remove_wp_version_rss');
+remove_action('wp_head', 'wp_generator');
+
+
+
+/**
+ * Removes file version queries from script/stylesheet calls.
+ * http://wordpress.stackexchange.com/a/96325/23011
+ * 
+ * Enhanced to keep query of google font stylesheets:
+ * Removes “?ver=3.5.1” from http://domain.tld/wp-content/themes/theme/stlye.css?ver=3.5.1
+ * Leaves http://fonts.googleapis.com/css?family=MyFont untouched.
+ */
+add_filter( 'script_loader_src', 'gp130419_remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', 'gp130419_remove_script_version', 15, 1 );
+function gp130419_remove_script_version( $src ){
+    $url = explode( '?', $src );
+
+    if( 'http://fonts.googleapis.com/css' == $url[0] ) :
+        $version = explode( '&ver=', $url[1] );
+        $url[1] = $version[0];
+    endif;
+
+    return ( 'http://fonts.googleapis.com/css' == $url[0] ) ? $url[0] . '?' . $url[1] : $url[0];
+
+}
+
+
 
 /**
  * Implement the Custom Header feature.
